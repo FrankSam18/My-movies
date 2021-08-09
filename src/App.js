8d2251de4e7afbe314c8d70c9b828f1be9f7e3d2
Frank Obuobi <you@example.com>
2021-08-09 22:50:29 +0000

    my movies

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Movies from "./Components/Movies";
+import './Components/Movies.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="head" style={{textAlign:"center",fontSmooth: "auto"}}>Phrank's Movie</h1>
+      <Movies/>
+    </div>
+  );
+}
+
+export default App;
